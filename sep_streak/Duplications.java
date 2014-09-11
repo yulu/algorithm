15d25d28136828e6f09e21f13e7e8f108945a81d
yulu <yulu8798@gmail.com>
2014-09-11 23:20:41 +0800

    sep 11

--- sep_streak/Duplications.java
+++ sep_streak/Duplications.java
@@ -2,28 +2,68 @@
  * Sep 10, cci 2.1
  */
 public class Duplications {
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		Node list = new Node(0);
+		for(int i = 0; i < 10; i++) {
+			list.appendToTail(i);
+		}
+		for(int i = 10; i > 0; i--) {
+			list.appendToTail(i);
+		}
+
+		list.print();
+
+		removeDup(list);
+		list.print();
 	}
 
-	class Node {
+	public static class Node {
 		int i;
 		Node next;
+		
+		public Node(int d){
+			i = d;
+		}
+
+		public void appendToTail(int d) {
+			Node end = new Node(d);
+			Node pt = this;
+			while(pt.next != null) {
+				pt = pt.next;
+			}
+			pt.next = end;
+		}
+
+		public void print() {
+			Node pt = this;
+			while(pt != null) {
+				System.out.print(pt.i);
+				pt = pt.next;
+			}
+			System.out.println();
+		}
 	}
 
 	public static void removeDup(Node head) {
-		Set tmp = new Set();
 		//start from the begining
 		Node pt = head;
 		Node prev = head;
-		tmp.add(head.i);
 
 		while( pt != null) {
-			//check dup
-			if(tmp.contains(pt.next.i)) {
-				prev.next = pt.next;	
-			}else{
+			Node tmp = head;
+			while (tmp != pt) {
+				//if find duplications
+				if(tmp.i == pt.i)
+					break;
+				tmp = tmp.next;
+			}
+			//if no dup is found
+			if(tmp == pt) {
 				prev = pt;
+			}else{
+			//if dup is found
+				prev.next = pt.next;
 			}	
 			pt = pt.next;
 		}
